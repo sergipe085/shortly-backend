21a758792cc89b38c511af4c083d5c90c4b378e9
Sérgio <71034184+serjolas1@users.noreply.github.com>
2021-12-31 10:08:56 -0300

    open link route

--- src/routes/link.routes.ts
+++ src/routes/link.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
 
 import createLinkcontroller from "../modules/links/useCases/createLink";
+import openLinkController from "../modules/links/useCases/openLink";
 
 const linkRoutes = Router();
 
 linkRoutes.post("/", (req, res) => {
     return createLinkcontroller().handle(req, res);
+});
+
+linkRoutes.get("/:code", (req, res) => {
+    return openLinkController().handle(req, res);
 });
 
 export { linkRoutes };
